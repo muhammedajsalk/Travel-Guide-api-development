ab901a91cbe7d0c25739c686b74f0c95f244035a
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-09-11 21:12:09 +0530

    Traveller App API(Place detail view)

--- api/v1/places/serializers.py
+++ api/v1/places/serializers.py
@@ -6,3 +6,9 @@ class PlaceSerializer(ModelSerializer):
     class Meta:
         fields = ("id", "name", "featured_image", "place")
         model = Place
+
+
+class PlaceDetailsSerializer(ModelSerializer):
+    class Meta:
+        fields = ("id", "name", "featured_image", "place", "description", "category")
+        model = Place
--- api/v1/places/views.py
+++ api/v1/places/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.v1.places.serializers import PlaceSerializer
+from api.v1.places.serializers import PlaceSerializer,PlaceDetailsSerializer
 from places.models import Place
 
 
@@ -20,3 +20,28 @@ def places(request):
     }
 
     return Response(response_data)
+
+
+@api_view(["GET"])
+def place(request,pk):
+    if Place.objects.filter(pk=pk).exists():
+        instances = Place.objects.get(pk=pk)
+    
+        context = {
+            "request":request
+        }
+        
+        serializer = PlaceDetailsSerializer(instances,context=context)
+        response_data = {
+            "status_code" : 6000,
+            "data" : serializer.data
+        }
+
+        return Response(response_data)
+    else:
+        response_data = {
+            "status_code" : 6001,
+            "data" : "Place not exist"
+        }
+
+        return Response(response_data)
