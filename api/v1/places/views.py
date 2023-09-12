8ddc828d4aa47351d0e3834acb87b3fc4d68ac76
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-09-12 14:31:49 +0530

    Search and Pagination(API Pagination)

--- api/v1/places/views.py
+++ api/v1/places/views.py
@@ -6,6 +6,7 @@ from api.v1.places.serializers import PlaceSerializer,PlaceDetailsSerializer
 from places.models import Place
 from django.db.models import Q
 
+from api.v1.places.pagination import StandardResultSetPagination
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
@@ -17,13 +18,22 @@ def places(request):
         ids = q.split(",")
         instances = instances.filter(category__in=ids)
 
+    paginator = StandardResultSetPagination()
+    paginated_result = paginator.paginate_queryset(instances,request)
+
     context = {
         "request":request
     }
     
-    serializer = PlaceSerializer(instances,many=True,context=context)
+    serializer = PlaceSerializer(paginated_result,many=True,context=context)
+
     response_data = {
         "status_code" : 6000,
+        "count" : paginator.page.paginator.count,
+        "links" : {
+             "next" : paginator.get_next_link(),
+             "previous" : paginator.get_previous_link()
+        },
         "data" : serializer.data
     }
 
