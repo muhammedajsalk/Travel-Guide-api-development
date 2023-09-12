ff20efc2843a74587432cf2c063b017cccbd3456
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-09-12 08:23:09 +0530

    Authentication(Generating Token)

--- traveller/settings.py
+++ traveller/settings.py
@@ -97,3 +97,12 @@ MEDIA_URL ="/media/"
 MEDIA_ROOT = BASE_DIR /"media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
--- traveller/urls.py
+++ traveller/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/places/', include("api.v1.places.urls")),
+    path('api/v1/auth/', include("api.v1.auth.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
